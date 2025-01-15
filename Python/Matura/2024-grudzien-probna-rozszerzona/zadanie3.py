30424c35c8c8d4aca937013ffc8b3ec9f44741d5
Ocelot78 <wojtekkonieczny1@gmail.com>
2025-01-15 20:15:14 +0100

    1

--- Python/Matura/2024-grudzien-probna-rozszerzona/zadanie3.py
+++ Python/Matura/2024-grudzien-probna-rozszerzona/zadanie3.py
@@ -1,41 +1,62 @@
 from math import sqrt
-with open("liczby_przyklad.txt","r") as file:
-    data = file.read().split()
-def isPrime(a):
-    for j in range(2,int(a/2)+1):
-        if a % j == 0:
+
+def process_number(number):
+    digits = sorted(str(number))
+    max_number = int(''.join(digits[::-1]))
+    min_number = int(''.join(digits))
+    return max_number - min_number
+
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
             return False
     return True
-found = False
-counter = 0
-with open("wyniki3.txt","w") as result:
-    result.write("Zadanie 3\n")
-    for i in data:
-        prime_counter = 0
-        i = int(i)
-        if sqrt(i) == int(sqrt(i)):
-            counter += 1
-            if found == False:
-                result.write(f"Odpowiedza jest liczba {i}")
-                found = True
-                
-        for k in range(2,int(i/2)+1):
-            if isPrime(k):
-                if i % k == 0:
-                    prime_counter += 1
-                    if prime_counter == 5:
-                        result.write(f"\n{i}")
-        i = str(i)
-        minNumber = ''.join(sorted(i))
-        maxNumber = minNumber[::-1]
-        maxNumber = int(maxNumber)
-        minNumber = int(minNumber)
-        i = int(i)
-        sub = maxNumber - minNumber
-        diff = i /sub
-        if diff == 0:
-            print(f"Liczba {i} roznica {sub} jest rowna liczbie {i}")
-        elif int(diff) >0:
-            print(f"roznica {sub} jest mniejsza o {int(diff)} razy od {i}")
-    result.write(f"\nIstnieje takich liczb: {counter}")
-    
+
+def process_numbers():
+    less, greater, equal = 0, 0, 0
+    equals_list = []
+    perfect_squares = []
+    results = []
+
+    with open('liczby.txt') as f:
+        for line in f:
+            num = int(line.strip())
+            diff = process_number(num)
+
+            if diff < num:
+                less += 1
+            elif diff > num:
+                greater += 1
+            else:
+                equal += 1
+                equals_list.append(num)
+
+            if sqrt(num).is_integer():
+                perfect_squares.append(num)
+
+            divisor_count = 0
+            for k in range(2, num // 2 + 1):
+                if num % k == 0 and is_prime(k):
+                    divisor_count += 1
+                    if divisor_count >= 5:
+                        results.append(num)
+                        break
+
+    with open("wyniki3.txt", "w") as out:
+        out.write("Zadanie 3\n")
+        if perfect_squares:
+            out.write(f"Odpowiedz to liczba {perfect_squares[0]}\n")
+        out.write(f"Liczb takich jest: {len(perfect_squares)}\n")
+        for result in results:
+            out.write(f"\n{result}")
+
+    with open("zadanie3.txt", "w") as out:
+        out.write(f'Liczba roznic mniejszych od liczby: {less}\n')
+        out.write(f'Liczba roznic wiekszych od liczby: {greater}\n')
+        out.write(f'Liczba roznic rownych liczbie: {equal}\n')
+        out.write('Liczby, dla ktorych roznica jest rowna liczbie:\n')
+        for number in equals_list:
+            out.write(f'{number}\n')
+
+process_numbers()
